5c1ba876e334b3b3ff3a076f150247d81044251e
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-21 22:06:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./frontend/css/main.css">
+
+    <!-- bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <nav class="d-flex justify-content-between container">
+        <div>
+            <img src="./frontend/images/logo.svg" class="logo" alt="logo icon">
+        </div>
+        <div class="links-container">
+            <ul class="d-flex ">
+                <li class="me-5">
+                    <a href="#">Contact</a>
+                </li>
+                <div class="container d-flex test">
+                    <div>
+                        <img src="./frontend/images/icon-user.svg" class="user-icon" alt="">
+                    </div>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+    <!-- end of navbar -->
+
+    <!-- Home -->
+
+    <!-- End of Home -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
