9280eb177d4ea44947ad175a38d055d6a0498870
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-23 14:24:37 +0100

    Woking on UI

--- index.php
+++ index.php
@@ -1,29 +1,39 @@
 <?php include "frontend/head.php"; ?>
 
-<!-- navbar -->
-<nav class="d-flex justify-content-between container mt-3">
-    <div>
-        <img src="./frontend/images/logo.svg" class="logo" alt="logo icon">
-    </div>
-    <div class="links-container">
-        <ul class="d-flex ">
-            <li class="me-5">
-                <a href="#">Contact</a>
-            </li>
-            <div class="d-flex align-items-center">
-                <div class="user-icon">
-                    <img src="./frontend/images/icon-user.svg" class="user" alt="">
-                </div>
-                <li class="ms-5 login">
-                    <a href="#">Login</a>
-                </li>
-            </div>
-        </ul>
-    </div>
-</nav>
-<!-- end of navbar -->
+<!-- container -->
+<div class="container">
 
-<!-- Home -->
+    <!-- navbar -->
+    <nav class="d-flex justify-content-between container mt-3">
+        <div>
+            <img src="./frontend/images/logo.svg" class="logo-icon" alt="logo icon">
+        </div>
+        <div class="links-container">
+            <ul class="d-flex ">
+                <li class="me-5">
+                    <a href="#">Contact</a>
+                </li>
+                <div class="d-flex align-items-center">
+                    <div class="user-icon d-flex justify-content-center align-items-center">
+                        <img src="./frontend/images/icon-user.svg" class="user" alt="">
+                    </div>
+                    <li class="ms-5 login">
+                        <a href="#">Login</a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+    <!-- end of navbar -->
+
+    <!-- Home -->
+
+    <section class="home">
+        <h4 class="subtitle">read the latest articles</h4>
+        <h1 class="title"><span class="logo">blog.ger</span> express your <br>opinion with us</h1>
+    </section>
+
+</div>
 
 <!-- End of Home -->
 
