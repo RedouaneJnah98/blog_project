f2d234eec583a9b532132ac23873df9b0de754c0
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-23 22:26:15 +0100

    fix UI bugs

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
-<?php include "frontend/head.php"; ?>
+<?php include_once "frontend/head.php"; ?>
 
 <!-- container -->
-<div class="container">
-
-    <!-- navbar -->
-    <nav class="d-flex justify-content-between container mt-3">
-        <div>
-            <img src="./frontend/images/logo.svg" class="logo-icon" alt="logo icon">
-        </div>
-        <div class="links-container">
-            <ul class="d-flex ">
-                <li class="me-5">
-                    <a href="#">Contact</a>
-                </li>
-                <div class="d-flex align-items-center">
-                    <div class="user-icon d-flex justify-content-center align-items-center">
-                        <img src="./frontend/images/icon-user.svg" class="user" alt="">
-                    </div>
-                    <li class="ms-5 login">
-                        <a href="#">Login</a>
-                    </li>
+<!-- navbar -->
+<!-- <nav class="d-flex justify-content-between mt-3">
+    <div>
+        <img src="./frontend/images/logo.svg" class="logo-icon" alt="logo icon">
+    </div>
+    <div class="links-container">
+        <ul class="d-flex ">
+            <li class="me-5">
+                <a href="#">Contact</a>
+            </li>
+            <div class="d-flex align-items-center">
+                <div class="user-icon d-flex justify-content-center align-items-center">
+                    <img src="./frontend/images/icon-user.svg" class="user" alt="">
                 </div>
-            </ul>
-        </div>
-    </nav>
-    <!-- end of navbar -->
+                <li class="ms-5 login">
+                    <a href="#">Login</a>
+                </li>
+            </div>
+        </ul>
+    </div>
+</nav> -->
+<!-- end of navbar -->
 
-    <!-- Home -->
+<!-- Home -->
 
-    <section class="home">
+<div class="hero-container home">
+    <div>
         <h4 class="subtitle">read the latest articles</h4>
         <h1 class="title"><span class="logo">blog.ger</span> express your <br>opinion with us</h1>
-    </section>
-
+    </div>
 </div>
+
 
 <!-- End of Home -->
 
