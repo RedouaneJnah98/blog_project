f10c520deda0b27fcfc9caf392535d769119cc35
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-10-08 12:11:03 +0100

    fix login system bugs

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,15 @@
-<?php include "./components/adminHeader.php"; ?>
+<?php
+include "./components/adminHeader.php";
+session_start();
 
-<h1>hello this the index page</h1>
+if (isset($_SESSION["username"])) {
+    $userName = $_SESSION["username"];
+    echo "<h3>Welcom $userName</h3>";
+?>
+<a href="utils/logout.inc.php">Logout</a>
+
+<?php } ?>
+
+
 
 <?php include "./components/adminFooter.php"; ?>
